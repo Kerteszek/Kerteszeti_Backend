0340f8f0987d1042b9fc680d9bbe1cfcd2e47b6b
DVMaja <majadreilinger@gmail.com>
2024-02-20 11:45:24 +0100

    api routok vissza kerültek

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth.basic'])->group(function () {
+
+
+    Route::middleware(['admin'])->group(function () {
+        //elérheti az admin, és a superAdmin
+        Route::apiResource('/users', UserController::class);
+    });
+
+    Route::middleware(['superadmin'])->group(function () {
+
+        //Csak a superadmin érheti el
+
+
+    });
 });
