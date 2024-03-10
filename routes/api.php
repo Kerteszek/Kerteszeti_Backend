55e59dd0404790131963637b4c1f45c264eca5d3
DVMaja <majadreilinger@gmail.com>
2024-03-10 20:43:16 +0100

    SuperAdmin koilistázza a felhasználókat

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PictureController;
+use App\Http\Controllers\PlantController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -29,13 +30,18 @@ Route::middleware(['auth.basic'])->group(function () {
     Route::middleware(['admin'])->group(function () {
         //elérheti az admin, és a superAdmin
         Route::apiResource('/users', UserController::class);
-        //Route::post('products', [ProductController::class, 'store']);
+        //Termék Update és Patch
+        Route::patch('products/{product_id}', [ProductController::class, 'update']);
+        Route::post('products', [ProductController::class, 'store']);
+
+        Route::patch('plants/{scientific_name}', [PlantController::class, 'update']);
+        Route::post('plants', [PlantController::class, 'store']);
     });
 
     Route::middleware(['superadmin'])->group(function () {
         //Csak a superadmin érheti el
-
-
+        //Route::delete('/products/{product_id}', [PlantController::class, 'destroy']);
+        Route::get('users', [UserController::class, 'index']);
     });
 });
 
@@ -44,6 +50,9 @@ Route::get('products', [ProductController::class, 'index']);
 
 //Termék összes frontendremenő adata
 Route::get('product_frontend', [ProductController::class, 'frontendTermek']);
+//frontendTermekKeppel
+Route::get('product_w_pictures', [ProductController::class, 'frontendTermekKeppel']);
+
 
 //Képek elérési útja
 Route::get('pictures', [PictureController::class, 'index']);
