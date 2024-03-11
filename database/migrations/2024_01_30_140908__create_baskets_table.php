7450450f6b185e546ad6d2eb3710e8f4aab68500
DVMaja <majadreilinger@gmail.com>
2024-03-11 17:54:56 +0100

    basket próba adatok

--- database/migrations/2024_01_30_140908__create_baskets_table.php
+++ database/migrations/2024_01_30_140908__create_baskets_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,10 +13,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('baskets', function (Blueprint $table) {
-            $table->integer('basket_id')->autoIncrement()->startingValue(100000);
+            $table->intege()->startingValue(100000);
             $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
         });
+
+        DB::table('purchases')->insert([
+            ['user_id' => 1],
+            ['user_id' => 2],
+            ['user_id' => 2],
+            ['user_id' => 3],
+            ['user_id' => 2],
+        ]);
     }
 
     /**
