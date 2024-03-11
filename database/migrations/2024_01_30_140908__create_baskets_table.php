07cf1a61d12f10a1ee3998c558e54dce2a25fe5c
DVMaja <majadreilinger@gmail.com>
2024-03-11 18:05:48 +0100

    mostmár tényleg javítva van

--- database/migrations/2024_01_30_140908__create_baskets_table.php
+++ database/migrations/2024_01_30_140908__create_baskets_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
-        DB::table('purchases')->insert([
+        DB::table('baskets')->insert([
             ['user_id' => 1],
             ['user_id' => 2],
             ['user_id' => 2],
