8323d8498bd134d7bb4d5d902037484c2428a7c3
DVMaja <majadreilinger@gmail.com>
2024-03-22 13:54:33 +0100

    frontend bejelentkezéshez, elv van jobb megoldas

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'api/purchase_items/',
-        'api/purchase_items/*'
+        'api/purchase_items/*',
+        '/login'
     ];
 }
